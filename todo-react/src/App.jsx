97192577242120c5daa7709eed417253797ce71e
David Duranty <dadaduranty@gmail.com>
2023-10-13 11:05:05 +0200

    add css

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,14 +1,28 @@
 import "./App.css";
 
 function App() {
+  
+
   return (
-    <div className="container">
-      <h1>TODO LIST</h1>
-      <form className="input">
-        <input className="value" type="text" placeholder="Ma Todo List" />
+    <div className="main">
+      <div className="container">
+        <h1>Validation ToDo</h1>
         <input className="submit" type="submit" />
-      </form>
-      <span></span>
+        <li></li>
+      </div>
+      <div className="container">
+        <h1>TODO LIST</h1>
+        <form className="input">
+          <input className="value" type="text" placeholder="Ma Todo List" />
+          <input className="submit" type="submit" />
+        </form>
+        <li></li>
+      </div>
+      <div className="container">
+        <h1>Effacer ToDo</h1>
+        <input className="submit" type="submit" />
+        <li></li>
+      </div>
     </div>
   );
 }
